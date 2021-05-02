4c0423736abdc5030df5c35a8b4a84eb17dfcdd6
Yash <yashyenugu@gmail.com>
2021-05-02 21:08:00 +0530

    Added image filtering

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -12,9 +12,19 @@ const storage = multer.diskStorage({
     }
 })
 
+const imageFilter = (req, file, cb) => {
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
 //new Date().toISOString().replace(/:/g, '-') required because windows OS does not accept path with ':'
 
-const upload = multer({storage: storage});
+const upload = multer({storage: storage, limits: {
+    fileSize: 1024 * 1024 * 100
+}});
 const router = express.Router();
 
 router.post("/createPost", upload.single('postImage'), (req,res, next) => {
