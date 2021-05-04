0d45a146bb8eac98e2d5243bea54e7256a05accc
Yash <yashyenugu@gmail.com>
2021-05-05 00:17:34 +0530

    Added form validation for login

--- client/src/components/Login/LoginForm.jsx
+++ client/src/components/Login/LoginForm.jsx
@@ -1,77 +1,76 @@
-import React, {useState, useContext} from "react";
+import React, { useState, useContext } from "react";
 import axios from "axios";
-import {useHistory}  from "react-router-dom";
-import {AuthContext} from "../contextProviders/authContext";
+import { useHistory } from "react-router-dom";
+import { AuthContext } from "../contextProviders/authContext";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const LoginForm = () => {
 
     const history = useHistory();
 
-    const {toggleAuth} = useContext(AuthContext);
+    const { toggleAuth } = useContext(AuthContext);
 
-    const [input, setInput] = useState({
-        username:"",
-        password:""
-    });
-
-    const handleInput = (event) => {
-        setInput(prev => {
-            return {
-                ...prev,
-                [event.target.name]: event.target.value
-            }
-        })
-    };
-    
-    const login = (event) => {
-
-        axios.post("/api/login",input)
-        .then(res => {
-            localStorage.setItem("accessToken",res.data.accessToken);
-            localStorage.setItem("refreshToken",res.data.refreshToken);
-            toggleAuth(true);
-            history.push("/");
-        })
-        .catch(err => {
-            console.log(err);
-        });
-
-        event.preventDefault();
-    }
 
     return (
         <div className="loginForm container mx-auto w-full max-w-md bg-gray-900">
-            <form className="px-10 py-10" onSubmit={login}>
-                <div>
-                <h1 className="text-center logo">NeonGram</h1>
-                </div>
-                <div className="mb-3 mt-10 px-3">
-                    <input
-                        type="text" 
-                        placeholder="username" 
-                        name="username"
-                        onChange={handleInput}
-                        value={input.username}
-                        className="rounded text-white bg-gray-800 px-2 py-2 transition duration-150 ease-in-out border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent w-full"
+            <Formik
+                initialValues={{
+                    username: "",
+                    password: ""
+                }}
+                validate={values => {
+                    const errors = {};
+                    if (!values.username) {
+                        errors.username = 'username required'
+                    }
+                    if (!values.password) {
+                        errors.password = 'password required'
+                    }
+                    return errors;
+                }}
+                validateOnMount = {false}
+                onSubmit={(values, { setSubmitting }) => {
+                    setSubmitting(true);
+                    console.log(values);
+                    axios.post("/api/login", {
+                        username: values.username,
+                        password: values.password
+                    })
+                        .then(res => {
+                            localStorage.setItem("accessToken", res.data.accessToken);
+                            localStorage.setItem("refreshToken", res.data.refreshToken);
+                            toggleAuth(true);
+                            history.push("/");
+                        })
+                        .catch(err => {
+                            console.log(err);
+                        });
+                    setSubmitting(false);
+                }}
+            >
+                {({ isSubmitting,isValid,dirty }) => (
+                    <Form className="px-10 py-10">
+                        <div>
+                            <h1 className="text-center logo">NeonGram</h1>
+                        </div>
+                        <div className="mb-3 mt-10 px-3">
+                            <Field type="text" name="username" placeholder="username" className="rounded text-white bg-gray-800 px-2 py-2 transition duration-150 ease-in-out border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent w-full" />
+                            {/* <ErrorMessage name="username" component="p" className="text-red-600 text-xs italic mt-1 ml-1" /> */}
+                        </div>
+                        
+                        <div className="mb-5 px-3">
+                            <Field type="password" name="password" placeholder="password" className="rounded text-white bg-gray-800 px-2 py-2 transition duration-150 ease-in-out border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent w-full" />
+                            {/* <ErrorMessage name="password" component="p" className="text-red-600 text-xs italic mt-1 ml-1" /> */}
+                        </div>
+                        
+                        <div>
+                            <button type="submit" disabled={isSubmitting || !(isValid && dirty)} className="w-full bg-purple-800 hover:bg-purple-900 disabled:opacity-50 disabled:cursor-not-allowed transition duration-150 ease-in-out text-white rounded-full py-2 focus:outline-none">Sign In</button>
+                        </div>
 
-                    />
-                </div>
+                    </Form>
+                )}
 
-                <div className="mb-5 px-3">
-                    <input 
-                        type="password" 
-                        placeholder="password"
-                        name="password"
-                        onChange={handleInput}
-                        value={input.password}
-                        className="rounded text-white bg-gray-800 px-2 py-2 mb-2 transition duration-150 ease-in-out border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent w-full"
-                    />
-                </div>
-                <div>
-                    <button type="submit" className="w-full bg-purple-800 hover:bg-purple-900 transition duration-150 ease-in-out text-white rounded-full py-2 focus:outline-none">Sign In</button>
-                </div>
-
-            </form>
+            </Formik>
 
         </div>
     )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     variants: {
       extend: {
         opacity: ['disabled'],
+        cursor: ['disabled']
       }
     },
     
