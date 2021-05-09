07f1b9df45288aa1679df807498ff53db4b6b832
Yash <yashyenugu@gmail.com>
2021-05-09 16:21:27 +0530

    Added image gallery

--- client/src/components/user/PostGallery.jsx
+++ client/src/components/user/PostGallery.jsx
@@ -3,8 +3,15 @@ import React from "react";
 const PostGallery = ({posts}) => {
 
     return (
-        <div>
-            a
+        <div className="max-w-4xl text-center grid grid-cols-3 gap-6 mx-auto w-full mb-6">
+        {posts && posts.map(post => (
+            <div className="h-72">
+                <img src={post.postImage} alt={post.text} key={post._id} className="object-cover h-72 w-full"/>
+            </div>
+            
+
+        ))}
+            
         </div>
     )
 }
