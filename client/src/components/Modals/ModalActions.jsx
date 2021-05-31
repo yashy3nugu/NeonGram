bcedb94c3e95ac1a1c0815a72b81e3c47bfac2e1
Yash <yashyenugu@gmail.com>
2021-05-31 22:40:15 +0530

    Added delete button for post modal

--- client/src/components/Modals/ModalActions.jsx
+++ client/src/components/Modals/ModalActions.jsx
@@ -6,9 +6,11 @@ import ThumbUpIcon from "../Icons/ThumbUpIcon";
 import BookMarkIcon from "../Icons/BookMarkIcon";
 import PlusIcon from "../Icons/PlusIcon";
 import UserIcon from "../Icons/UserIcon";
+import DeleteIconSolid from '../Icons/DeleteIconSolid';
 import { AuthContext } from "../contextProviders/authContext";
 import axios from "axios";
 import { Formik, Form, Field } from 'formik';
+import ButtonSpinner from '../Icons/ButtonSpinner';
 
 const ModalActions = ({ post, addComment, onDelete }) => {
 
@@ -20,6 +22,7 @@ const ModalActions = ({ post, addComment, onDelete }) => {
 
     const [numLikes, setNumLikes] = useState(post.likes.length);
     const [numDislikes, setNumDislikes] = useState(post.dislikes.length);
+    const [loading, setLoading] = useState(false)
 
     const handleLiked = (e) => {
 
@@ -119,14 +122,20 @@ const ModalActions = ({ post, addComment, onDelete }) => {
 
     return (
         <>
-            <div className="my-2">
+            <div className="my-2 flex">
                 {post.user.profilePicture ? (
                     <img className="w-8 mr-2 rounded-full inline" src={post.user.profilePicture} alt={post.user.username} />
                 ) : (
                     <UserIcon className="w-8 mr-2 inline text-gray-200" />
                 )}
-                <a href={`/user/${post.username}`} className="text-gray-200 text-base font-normal">{post.username}</a>
-                <button onClick={() => onDelete(post._id)}>Delete</button>
+                <a href={`/user/${post.user.username}`} className="text-gray-200 text-base font-normal">{post.user.username}</a>
+                {auth._id === post.user._id && (
+                    <button className="text-right ml-auto text-neon-red" onClick={() => {
+                        onDelete(post._id);
+                        setLoading(true);
+                        }}>{loading && <ButtonSpinner className="animate-spin w-6 inline"/> } <DeleteIconSolid className="w-6 inline"/></button>
+                )}
+                
             </div>
             <div className="text-gray-400 mt-2 mb-8">
                 <p>{post.text}</p>
@@ -190,7 +199,7 @@ const ModalActions = ({ post, addComment, onDelete }) => {
                 {({ isSubmitting, isValid, dirty }) => (
                     <Form autoComplete="off" className="flex mb-4">
                         <Field type="text" maxLength="600" name="comment" placeholder="Add a comment..." className="w-full bg-gray-800 rounded-lg mr-2 px-2 py-2 text-base text-gray-300 focus:outline-none" />
-                        <button type="submit" disabled={isSubmitting || !(isValid && dirty)} className="w-10 text-neon-green disabled:opacity-50 disabled:cursor-not-allowed"><PlusIcon className="" /></button>
+                        <button type="submit" disabled={isSubmitting || !(isValid && dirty)} className="w-10 text-neon-green disabled:opacity-50 disabled:cursor-not-allowed">{isSubmitting ? <ButtonSpinner className="animate-spin"/> : <PlusIcon className="" />}</button>
                     </Form>
                 )}
 
