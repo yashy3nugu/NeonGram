469faad2b2a6d37653c1b5786bee56c8953a537e
yash <yashyenugu@gmail.com>
2022-08-07 13:45:41 +0530

    Cleaned post routes

---
+++ controllers/commentController.js
@@ -0,0 +1,28 @@
+const { authenticateToken } = require("../utils/jwt");
+const { Comment } = require("../models/commentModel");
+
+exports.addComment = (req, res) => {
+  const { postID } = req.params;
+  console.log(postID);
+
+  Comment.create(
+    { content: req.body.content, post: postID, user: req.user._id },
+    (err) => {
+      if (err) {
+        res.sendStatus(500);
+      } else {
+        res.sendStatus(201);
+      }
+    }
+  );
+};
+
+exports.getCommentsForPost = (req, res, next) => {
+  const { postID } = req.params;
+
+  Comment.find({ post: postID })
+    .populate({ path: "user", select: ["username", "profilePicture"] })
+    .then((comment) => {
+      res.send(comment);
+    });
+};
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,53 +1,14 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const mongoose = require("mongoose");
 const { authenticateToken } = require("../utils/jwt");
-const { Post } = require("../models/postModel");
-const { User } = require("../models/userModel");
 const { Comment } = require("../models/commentModel");
+const { commentController } = require("../controllers/commentController");
 
 const router = express.Router();
 
 // post a comment for a post
-router.post("/add/:postID", authenticateToken, (req, res, next) => {
-    const {postID} = req.params;
-    console.log(postID)
-
-    Comment.create({content: req.body.content, post: postID, user: req.user._id}, (err) => {
-        if(err) {
-            res.sendStatus(500);
-            
-        } else {
-            res.sendStatus(201);
-            
-        }
-    })
-});
+router.post("/add/:postID", authenticateToken, commentController.addComment);
 
 // get comments for a post
-router.get("/:postID", authenticateToken, (req, res, next) => {
-
-    const {postID} = req.params;
-
-    // Comment.find({post: postID}, (err, foundComments) => {
-    //     if(err) {
-    //         res.sendStatus(500);
-    //         
-    //     } else {
-    //         console.log(foundComments)
-    //         res.send(foundComments);
-    //         
-    //     }
-    // })
-
-    Comment.find({post: postID})
-    // .populate('user', 'username')
-    .populate({path: 'user', select: ['username','profilePicture']})
-    .then( comment => {
-        res.send(comment);
-        
-    })
-
-})
+router.get("/:postID", authenticateToken.commentController.getCommentsForPost);
 
 module.exports = router;
