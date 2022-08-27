2b83a7a45159190a9bc366d850b1512c41168f42
yash <yashyenugu@gmail.com>
2022-08-27 16:04:35 +0530

    Add async await for comment controllers

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,28 +2,33 @@ const { Comment } = require("../models/commentModel");
 
 /////////////////////////////////////////////////////
 // Add comment to a post
-exports.addComment = (req, res) => {
-  const { postID } = req.params;
-  Comment.create(
-    { content: req.body.content, post: postID, user: req.user },
-    (err) => {
-      if (err) {
-        res.sendStatus(500);
-      } else {
-        res.sendStatus(201);
-      }
-    }
-  );
+exports.addComment = async (req, res) => {
+  try {
+    const { postID } = req.params;
+    const comment = await Comment.create({
+      content: req.body.content,
+      user: req.user,
+      post: postID,
+    });
+    res.status(201).json(comment);
+  } catch (err) {
+    res.sendStatus(500);
+  }
 };
 
 /////////////////////////////////////////////////////
 // Get comments for a post
-exports.getCommentsForPost = (req, res, next) => {
-  const { postID } = req.params;
+exports.getCommentsForPost = async (req, res) => {
+  try {
+    const { postID } = req.params;
 
-  Comment.find({ post: postID })
-    .populate({ path: "user", select: ["username", "profilePicture"] })
-    .then((comment) => {
-      res.send(comment);
+    const comments = await Comment.find({ post: postID }).populate({
+      path: "user",
+      select: ["username", "profilePicture"],
     });
+
+    res.send(comments);
+  } catch (err) {
+    res.sendStatus(500);
+  }
 };
