57e0b42c917dc9746362366cfa7c6e10f9774699
yashyenugu <yashyenugu@gmail.com>
2022-09-03 18:12:23 +0530

    Make settings page responsive

--- client/src/components/Settings/ProfileSettings.jsx
+++ client/src/components/Settings/ProfileSettings.jsx
@@ -2,31 +2,30 @@ import React from "react";
 
 import ProfilePicChanger from "./ProfilePicChanger";
 
-import { Box, HStack, Text } from "@chakra-ui/react";
+import { Box, Stack, Text } from "@chakra-ui/react";
 
 import DetailsForm from "./DetailsForm";
 
 const ProfileSettings = () => {
   return (
     <Box
-      mt={50}
-      mb={50}
+      mt={{ md: 50 }}
+      mb={{ md: 50 }}
       bg={"primary.900"}
-      maxWidth="3xl"
+      w={{ base: "full", md: "sm", lg: "md", xl: "lg" }}
       mx="auto"
       px={10}
       py={10}
-      borderWidth="1px"
-      borderRadius="xl"
+      borderWidth={{ md: "1px" }}
+      borderRadius={{ md: "xl" }}
     >
       <Text fontSize="4xl" fontWeight="semibold" mb={3}>
         Settings
       </Text>
-      <HStack spacing={12} alignItems="">
+      <Stack direction={{base:"column-reverse", lg: "row"}} spacing={{base:5, lg:12}} alignItems="">
         <DetailsForm />
-
         <ProfilePicChanger />
-      </HStack>
+      </Stack>
     </Box>
   );
 };
--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -2,20 +2,8 @@ import React from "react";
 
 import ProfileSettings from "./ProfileSettings";
 
-import { Box } from "@chakra-ui/react";
-
 const Settings = () => {
-  return (
-    // <div className="flex flex-col lg:flex-row container mx-auto bg-gray-900 p-3">
-    //   <Sidebar changeSetting={changeSetting} selected={setting} />
-
-    //   {setting === "Profile" && <ProfileSettings />}
-    //   {setting === "Account" && <AccountSettings />}
-    // </div>
-    <Box>
-      <ProfileSettings />
-    </Box>
-  );
+  return <ProfileSettings />;
 };
 
 export default Settings;
