801d5f58dbda0aebed5f8fa01e85043a7d3c7955
Yash Yenugu <68002151+yashyenugu@users.noreply.github.com>
2022-09-07 00:35:41 +0530

    Delete client/src/components/shared directory

--- client/src/components/shared/ui/AppFormField.jsx
+++
@@ -1,8 +0,0 @@
-import { Input } from "@chakra-ui/react";
-import { Field } from "formik";
-
-const AppFormField = ({ ...props }) => {
-  return <Field as={Input} focusBorderColor="tertiary" bg="whiteAlpha.50" {...props} />;
-};
-
-export default AppFormField;
